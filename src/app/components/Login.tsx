c214872321f207046022b5a8d1f120100eda5801
Mahadi Hasan Sourav <sourav@chaldal.net>
2022-05-29 19:38:26 +0600

    fix login style and some others design

--- src/app/components/Login.tsx
+++ src/app/components/Login.tsx
@@ -28,46 +28,46 @@ const Login = () => {
   };
 
   return (
-    <section className="vh-100" style={{ backgroundColor: "#fff" }}>
+    <section className="vh-100 login" style={{ backgroundColor: "#263238" }}>
       <div className="row d-flex justify-content-center align-items-center h-100">
         <div className="col-12 col-md-8 col-lg-6 col-xl-5">
           <div className="card shadow-2-strong" style={{ borderRadius: 1 }}>
             <div className="card-body p-5">
               <form onSubmit={(e) => onSubmitLogin(e)}>
-                <div
-                  style={{
-                    display: "flex",
-                    justifyContent: "space-between",
-                    alignItems: "center",
-                  }}
-                >
-                  <h2 className="text-center">Login</h2>
-                </div>
-                <div className="form-group mb-2">
+                <h2 className="text-center mb-2">Login</h2>
+                <div className="input-group mb-2">
+                  <span className="input-group-text" id="basic-addon1">
+                    <i className="bi bi-envelope-fill"></i>
+                  </span>
                   <input
                     type="text"
                     className="form-control"
-                    placeholder="Enter email"
+                    placeholder="Email Address"
                     name="email"
                   />
                 </div>
-                <div className="form-group mb-2">
+                <div className="input-group mb-2">
+                  <span className="input-group-text" id="basic-addon1">
+                    <i className="bi bi-patch-exclamation-fill"></i>
+                  </span>
                   <input
                     type="password"
                     className="form-control"
-                    placeholder="Enter password"
+                    placeholder="Password"
                     name="password"
                   />
                 </div>
-                <button
-                  type="submit"
-                  className="btn btn-primary btn-block mt-2 mb-2"
-                >
-                  Login
-                </button>
-                <p style={{ color: "red", height: "10px" }}>
-                  {authState.loginErrorMsg}
-                </p>
+                <div className="text-center">
+                  <button
+                    type="submit"
+                    className="btn btn-primary btn-block mt-2 mb-2 "
+                  >
+                    LOG IN
+                  </button>
+                  <p style={{ color: "red", height: "10px" }}>
+                    {authState.loginErrorMsg}
+                  </p>
+                </div>
               </form>
             </div>
           </div>
